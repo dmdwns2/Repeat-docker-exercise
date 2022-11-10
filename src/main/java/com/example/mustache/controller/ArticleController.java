92d4b4f0f15dbc1708c40cd90b0da300f078bc2e
dmdwns2 <ssreki1@naver.com>
2022-11-10 13:27:55 +0900

    [SPRING MVC 복습] CRUD add 추가

--- src/main/java/com/example/mustache/controller/ArticleController.java
+++ src/main/java/com/example/mustache/controller/ArticleController.java
@@ -1,18 +1,34 @@
 package com.example.mustache.controller;
 
 
+import com.example.mustache.domain.Article;
+import com.example.mustache.dto.ArticleDto;
+import com.example.mustache.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/new")
-    public String createArticlePage(){
+    public String createArticlePage() {
         return "new";
     }
 
-
+    @PostMapping("")
+    public String createArticle(ArticleDto articleDto) {
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        return String.format("id:%d", savedArticle.getId());
+    }
 }
