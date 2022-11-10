863e246c887bc1dbcb225a974a4f831acf6c62e3
dmdwns2 <ssreki1@naver.com>
2022-11-10 13:14:03 +0900

    [SPRING MVC 복습] toEntity 추가

--- src/main/java/com/example/mustache/dto/ArticleDto.java
+++ src/main/java/com/example/mustache/dto/ArticleDto.java
@@ -1,6 +1,7 @@
 package com.example.mustache.dto;
 
 
+import com.example.mustache.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,8 @@ public class ArticleDto {
     private Long id;
     private String content;
     private String title;
+
+    public Article toEntity() {
+        return new Article(this.id, this.title, this.content);
+    }
 }
