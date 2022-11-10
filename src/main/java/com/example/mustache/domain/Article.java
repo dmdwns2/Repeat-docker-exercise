1d6f925b53389c433927b26faf3a0096c30bf0c2
dmdwns2 <ssreki1@naver.com>
2022-11-10 13:12:02 +0900

    [SPRING MVC 복습] persistence dependency 추가 , Article 추가

--- src/main/java/com/example/mustache/domain/Article.java
+++ src/main/java/com/example/mustache/domain/Article.java
@@ -1,4 +1,25 @@
 package com.example.mustache.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "article2")
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private String title;
 }
