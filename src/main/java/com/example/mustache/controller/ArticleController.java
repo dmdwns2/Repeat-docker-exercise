16b4bcc0433a5dc8602148ca06f68b451d98cff2
dmdwns2 <ssreki1@naver.com>
2022-11-10 11:38:12 +0900

    [SPRING MVC 복습] controller 및 new.mustach 생성

---
+++ src/main/java/com/example/mustache/controller/ArticleController.java
@@ -0,0 +1,18 @@
+package com.example.mustache.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+
+    @GetMapping("/new")
+    public String createArticlePage(){
+        return "new";
+    }
+
+
+}
